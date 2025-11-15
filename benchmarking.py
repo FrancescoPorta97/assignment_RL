da9969e6961f62d5bdaf926b7afccfbcff56902c
Francesco Porta <francescoporta@MinidiFrancesco.fritz.box>
2025-11-15 18:16:39 +0100

    changing benchmarking metrics

--- benchmarking.py
+++ benchmarking.py
@@ -16,7 +16,7 @@ def get_optimal_solution_cost(capacity, cost_matrix):
     C = np.repeat(cost_matrix, repeats=capacity, axis=0)
     rows, cols = linear_sum_assignment(C)
     cost = C[rows, cols].sum()
-    return cost
+    return float(cost)
 
 
 def get_greedy_solution_cost(capacity: int, cost_matrix: np.ndarray) -> float:
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     model_version = "20251105_135350"
     CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
     model_folder = os.path.join(CURRENT_DIR,"models", model_version)
-    NUM_RUNS = 1000
+    NUM_RUNS = 5000
 
     #ml initializations
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -80,6 +80,9 @@ if __name__ == "__main__":
     actor_model = RlModel(ActorConfig()).to(device)
     actor_model.load_state_dict(torch.load(os.path.join(model_folder, "actor.pt"), map_location=device))
 
+    n_optimal_soultion_found_greedy = 0
+    n_optimal_solution_found_rl = 0
+    costs_greedy , costs_rl, costs_opt = [], [], []
     for run_idx in range(NUM_RUNS):
         
         environment = Environment(
@@ -107,8 +110,32 @@ if __name__ == "__main__":
             action = int(masked_logits.argmax(dim=-1))
             environment.take_action(action)
         
-        cost_rl = get_assignment_cost(environment.cost_task_resource,
-                                    environment.assignment_solution)
+        cost_rl = float(get_assignment_cost(environment.cost_task_resource,
+                                    environment.assignment_solution))
         cost_opt = get_optimal_solution_cost(capacity, environment.cost_task_resource)
         cost_greedy = get_greedy_solution_cost(capacity, environment.cost_task_resource)
+
+        costs_opt.append(cost_opt)
+        costs_greedy.append(cost_greedy)
+        costs_rl.append(cost_rl)
+
+        if costs_greedy == costs_opt:
+            n_optimal_soultion_found_greedy += 1
+        
+        if cost_rl == cost_opt:
+            n_optimal_solution_found_rl += 1
+        
         print("Run idx:", run_idx)
+    
+    mean_cost_rl = (np.array(costs_rl)).mean()
+    mean_cost_opt = (np.array(costs_opt)).mean()
+    mean_cost_greedy = (np.array(costs_greedy)).mean()
+    
+    variance_cost_rl = (np.array(costs_rl)).var()
+    variance_cost_opt = (np.array(costs_opt)).var()
+    variance_cost_greedy = (np.array(costs_greedy)).var()
+
+    print(f"RL summary: n_times  found optimal soultion = {n_optimal_solution_found_rl} mean cost = {mean_cost_rl} variance cost = {variance_cost_rl}")
+    print(f"opt summary: mean cost = {mean_cost_opt} variance cost = {variance_cost_opt}")
+    print(f"greedy summary: n_times  found optimal soultion = {n_optimal_soultion_found_greedy} mean cost = {mean_cost_greedy} variance cost = {variance_cost_greedy}")
+    
